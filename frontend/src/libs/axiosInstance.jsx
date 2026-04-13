268d149e6eca5548c10c04e0ad3cb70e67dc15b8
Tanjila Islam <115866988+tanjila-i@users.noreply.github.com>
2026-04-13 20:25:47 +0600

    Update axiosInstance.jsx

--- frontend/src/libs/axiosInstance.jsx
+++ frontend/src/libs/axiosInstance.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "https://chitchat-backend-qns7.onrender.comi"
+      ? "https://chitchat-backend-g0bv.onrender.com"
       : "/api",
   withCredentials: true,
 });
