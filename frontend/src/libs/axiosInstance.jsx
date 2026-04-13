5ae48e6527d85ca0e4eaad3c638703bccf9b7234
Tanjila Islam <115866988+tanjila-i@users.noreply.github.com>
2026-04-13 20:49:36 +0600

    Update axios base URL for development mode

--- frontend/src/libs/axiosInstance.jsx
+++ frontend/src/libs/axiosInstance.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? "https://chitchat-backend-g0bv.onrender.com"
+      ? "https://chitchat-bac.onrender.com"
       : "/api",
   withCredentials: true,
 });
