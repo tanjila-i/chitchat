c48ef044fbff41e2b42991b590b80062fde60d09
Tanjila Islam <115866988+tanjila-i@users.noreply.github.com>
2026-04-13 20:00:37 +0600

    Update BASE_URL for production environment

--- frontend/src/stores/useAuthStore.js
+++ frontend/src/stores/useAuthStore.js
@@ -4,7 +4,7 @@ import { axiosInstance } from "../libs/axiosInstance";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+  import.meta.env.MODE === "development" ? "https://chitchat-backend-qns7.onrender.com" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
